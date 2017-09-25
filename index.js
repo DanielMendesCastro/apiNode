0d286f11fea0519a6578ade7f679f1a02475e671
DANIEL-PC\Daniel <daniel@engsolutions.com.br>
2017-09-25 18:26:59 -0300

    Adicionando model task

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import app from './config/express';
+import config from './config/env';
 
-app.listen(3000, () => {
-  console.log('API Server started and listening on port 3000');
+app.listen(config.port, () => {
+  console.log(`API Server started and listening on port ${config.port} (${config.env})`);
 });
 
 export default app;
